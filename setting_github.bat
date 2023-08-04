c2e0087854bd4b7b1e2d17c3b9d09561448d9f88
Daniel Prüher <daniel.prueher@siemens.com>
2023-08-04 09:13:39 +0300

    -- no description --

---
+++ setting_github.bat
@@ -0,0 +1,6 @@
+echo "# Project6" >> README.md
+git add README.md
+git branch -M main
+git remote add origin https://github.com/DanielMapSinHw/Project6.git
+git push -u origin main
+pause
---
+++ test_git_push1_lib/hdl/adder_behalf.vhd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d42bade6c7a1c57910c1a852482e003110151615682c5457eafc3d7b71ff2f38
+size 836
